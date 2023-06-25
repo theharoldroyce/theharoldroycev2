c7e77029bee28165fe1c2c5019e0ea703c76298c
theharoldroyce <theharoldroyce@yahoo.com>
2023-06-25 15:56:40 +0800

    01232- edit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
                         <h1 className="text-xl lg:2xl font-bold mb-4 text-[#00F4FB] animate-pulse">You Found Me!</h1>
                         <h1 className="text-2xl md:text-4xl font-bold mb-4">Hi, I'm Harold Royce</h1>
                         <h3 className=" text-md md:text-lg mb-4 text-[#FE025B] font-bold animate-pulse">Software Engineer | Web Developer</h3>
-                        <p className="text-lg mb-8">As a software engineer, I seamlessly integrate technology and design to craft immersive digital experiences. Leveraging my expertise in account management, frontend web development, and graphic design, I offer a distinctive approach to each project, resulting in captivating outcomes.</p>
+                        <p className="text-lg mb-8">I seamlessly integrate technology and design to craft immersive digital experiences. Leveraging my expertise in account management, frontend web development, and graphic design, I offer a distinctive approach to each project, resulting in captivating outcomes.</p>
                         <a className="hidden bg-[#FE025B] hover:bg-[#FEFFFE] text-white hover:text-[#FE025B] font-semibold py-2 px-4 rounded">Learn More</a>
                     </div>
                 </div>
